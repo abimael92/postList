7faa2cc852d7dca3d1c0d6fe5d19734ad1418f02
Abimael Garcia <Abimael.Garcia@improving.com>
2023-08-21 18:12:22 -0600

    first commit

---
+++ src/components/Post.tsx
@@ -0,0 +1,17 @@
+import classes from './Post.module.css';
+
+interface PostProps {
+	author: string;
+	body: string;
+}
+
+function Post({ author, body }: PostProps): JSX.Element {
+	return (
+		<li className={classes.post}>
+			<p className={classes.author}>{author}</p>
+			<p className={classes.text}>{body}</p>
+		</li>
+	);
+}
+
+export default Post;
