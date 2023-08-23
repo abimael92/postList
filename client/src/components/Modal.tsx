29acd8b699a4e0897b59c83406dfbf2362e979a1
Abimael Garcia <Abimael.Garcia@improving.com>
2023-08-22 19:18:37 -0600

    worked on backend

---
+++ client/src/components/Modal.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, MouseEventHandler } from 'react';
+import classes from './Modal.module.css';
+
+interface ModalProps {
+	children: ReactNode;
+	onClose: MouseEventHandler<HTMLDivElement>;
+}
+
+function Modal({ children, onClose }: ModalProps): JSX.Element {
+	return (
+		<>
+			<div className={classes.backdrop} onClick={onClose} />
+			<dialog open className={classes.modal}>
+				{children}
+			</dialog>
+		</>
+	);
+}
+
+export default Modal;
